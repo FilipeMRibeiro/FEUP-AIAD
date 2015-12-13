2a2958a7c6b78952d83e7c3d16a1d06873466f42
ferrolho <henriqueferrolho@gmail.com>
2015-12-13 03:03:43 +0000

    leadership prestige and capacity done

--- src/agents/SensingAgent.java
+++ src/agents/SensingAgent.java
@@ -3,6 +3,7 @@ package agents;
 import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -25,36 +26,40 @@ import utilities.Utilities;
 
 public class SensingAgent extends Agent implements Drawable {
 
-	private final static double minStdDeviation = 0.0005;
-	private final static double maxStdDeviation = 6;
+	private final static double MIN_STD_DEVIATION = 0.0005;
+	private final static double MAX_STD_DEVIATION = 6;
+
+	private final static float SECURITY_ENERGY_LEVEL = 1.5f;
+	private final static float MAXIMUM_ENERGY_LEVEL = 100f;
 
 	private final OurModel model;
 
 	private int x, y;
 	private State state;
 	private int sleepCountdown;
-	private float batteryLevel;
+	private float energyLevel;
 	private Color color;
 	private double stdDeviation, maxAdherence;
 	private ArrayList<Float> pollutionSamples;
 
 	private boolean leader;
-	private Set<AID> neighbours, dependantNeighbours;
+	private HashMap<AID, Double> neighboursAdherence;
+	private Set<AID> dependantNeighbours;
 	private AID leaderNodeOfMe;
 
 	public SensingAgent(int x, int y, OurModel model) {
 		this.x = x;
 		this.y = y;
 		this.state = State.ON;
-		this.batteryLevel = 100;
+		this.energyLevel = MAXIMUM_ENERGY_LEVEL;
 		this.color = Color.GREEN;
 		// TODO should this be random?
-		this.stdDeviation = Random.uniform.nextDoubleFromTo(minStdDeviation, maxStdDeviation);
+		this.stdDeviation = Random.uniform.nextDoubleFromTo(MIN_STD_DEVIATION, MAX_STD_DEVIATION);
 		this.maxAdherence = 0;
 		this.pollutionSamples = new ArrayList<Float>();
 
 		this.leader = false;
-		this.neighbours = new TreeSet<AID>();
+		this.neighboursAdherence = new HashMap<AID, Double>();
 		this.dependantNeighbours = new TreeSet<AID>();
 		this.leaderNodeOfMe = null;
 
@@ -71,7 +76,7 @@ public class SensingAgent extends Agent implements Drawable {
 
 				// TODO change this and make it a parameter for the gui
 				if (distance <= 10)
-					neighbours.add(sensor.getAID());
+					neighboursAdherence.put(sensor.getAID(), 0.0);
 			}
 		}
 	}
@@ -94,21 +99,21 @@ public class SensingAgent extends Agent implements Drawable {
 	private void update() {
 		switch (state) {
 		case ON:
-			if (batteryLevel > 0)
+			if (energyLevel > 0)
 				sampleEnvironment();
 
-			batteryLevel -= 0.1;
+			energyLevel -= 0.1;
 
-			if (batteryLevel <= 0) {
-				batteryLevel = 0;
+			if (energyLevel <= 0) {
+				energyLevel = 0;
 				state = State.OFF;
 			}
 
 			break;
 
 		case SLEEP:
-			if (batteryLevel <= 0) {
-				batteryLevel = 0;
+			if (energyLevel <= 0) {
+				energyLevel = 0;
 				state = State.OFF;
 			}
 
@@ -140,7 +145,7 @@ public class SensingAgent extends Agent implements Drawable {
 			e.printStackTrace();
 		}
 
-		for (AID aid : neighbours)
+		for (AID aid : neighboursAdherence.keySet())
 			msg.addReceiver(aid);
 
 		send(msg);
@@ -167,10 +172,15 @@ public class SensingAgent extends Agent implements Drawable {
 							if (message instanceof Sample) {
 								double receivedSample = message.getValue();
 
+								// System.out.println("Received sample: " +
+								// receivedSample);
+
 								double adherence = calcAdherence(receivedSample);
 
 								if (maxAdherence < adherence) {
 									maxAdherence = adherence;
+
+									neighboursAdherence.put(msg.getSender(), adherence);
 
 									try {
 										ACLMessage reply = msg.createReply();
@@ -182,12 +192,13 @@ public class SensingAgent extends Agent implements Drawable {
 										e.printStackTrace();
 									}
 								}
-
-								System.out.println("Received sample: " + receivedSample);
 							} else if (message instanceof Adherence) {
 								System.out.println("Received adherence.");
+								double receivedAdherence = message.getValue();
 
-								double leadership = 1;
+								double leadership = calcLeadership(receivedAdherence);
+
+								System.out.println("leadership: " + leadership);
 
 								try {
 									ACLMessage reply = msg.createReply();
@@ -283,12 +294,29 @@ public class SensingAgent extends Agent implements Drawable {
 						stdDeviation);
 
 		double eHj = Math.pow(Math.E, Utilities.entropy(stdDeviation));
-		double eHmin = Math.pow(Math.E, Utilities.entropy(minStdDeviation));
-		double eHmax = Math.pow(Math.E, Utilities.entropy(maxStdDeviation));
+		double eHmin = Math.pow(Math.E, Utilities.entropy(MIN_STD_DEVIATION));
+		double eHmax = Math.pow(Math.E, Utilities.entropy(MAX_STD_DEVIATION));
 
 		double varModelCertainty = 1 - ((eHj - eHmin) / (eHmax - eHmin));
 
 		return valsSimilarity * varModelCertainty;
+	}
+
+	private double calcLeadership(double negotiatingNeighbourAdherence) {
+		double prestigeSum = 0;
+
+		for (AID dependantAID : dependantNeighbours)
+			prestigeSum += neighboursAdherence.get(dependantAID);
+		prestigeSum += calcAdherence(getLastPollutionSample());
+		prestigeSum += negotiatingNeighbourAdherence;
+
+		double prestige = prestigeSum / (dependantNeighbours.size() + 2);
+
+		double capacity = (energyLevel - SECURITY_ENERGY_LEVEL) / MAXIMUM_ENERGY_LEVEL;
+
+		double representativeness = 1 / (Math.pow(Math.E, 1));
+
+		return prestige * capacity * representativeness;
 	}
 
 	private void sleep() {
@@ -306,9 +334,9 @@ public class SensingAgent extends Agent implements Drawable {
 			break;
 
 		case ON:
-			if (batteryLevel < 20)
+			if (energyLevel < 20)
 				color = Color.RED;
-			else if (batteryLevel < 60)
+			else if (energyLevel < 60)
 				color = Color.YELLOW;
 			else
 				color = Color.GREEN;
@@ -336,7 +364,7 @@ public class SensingAgent extends Agent implements Drawable {
 	}
 
 	public float getBatteryLevel() {
-		return batteryLevel;
+		return energyLevel;
 	}
 
 }
